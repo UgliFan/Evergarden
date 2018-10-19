bb43bcfda89cda1eba01cbf5e0bafa503efe1a06
fanyeqing <fanyeqing@bilibili.com>
2018-10-19 16:36:07 +0800

    save

--- common/config.js
+++ common/config.js
@@ -1,8 +1,8 @@
 module.exports = {
     mysql: {
-        host: '139.224.133.72',
-        user: 'uglifan',
-        password: 'Fyq910606',
-        port: '3306'
+        host: 'xxx',
+        user: 'xxx',
+        password: 'xxx',
+        port: 'xxx'
     }
 };
