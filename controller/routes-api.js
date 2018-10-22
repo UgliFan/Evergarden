8421a3fc27bad7ba93a926a91215ab105f71ba46
fanyeqing <fanyeqing@bilibili.com>
2018-10-22 18:46:55 +0800

    save

--- controller/routes-api.js
+++ controller/routes-api.js
@@ -2,6 +2,6 @@ const Router = require('koa-router');
 const router = new Router();
 
 const apiCtl = require('./api');
-router.use('/api', apiCtl.routes(), apiCtl.allowedMethods());
+router.use(apiCtl.routes(), apiCtl.allowedMethods());
 
 module.exports = router;
