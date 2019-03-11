c90372210daaeedd874f72eacb79c6be40e09cf8
fanyeqing <fanyeqing@bilibili.com>
2019-03-11 19:01:53 +0800

    chart pie

--- controller/api/chart.js
+++ controller/api/chart.js
@@ -1,11 +1,57 @@
 const Router = require('koa-router');
 const route = new Router();
+const mysqlInstance = require('../../common/mysql');
 
 route.get('/pie', async ctx => {
-    ctx.body = {
-        code: 0,
-        result: []
-    };
+    const query = ctx.query || {};
+    if (query.y && query.m) {
+        const coltName = `tally_${query.y}_${query.m}`;
+        const isExsit = await mysqlInstance.EXIST_TABLE(coltName);
+        if (isExsit) {
+            const select = await mysqlInstance.SELECT(coltName, {
+                columns: [
+                    'cid',
+                    '`name`',
+                    'SUM(summary) AS `sum`'
+                ],
+                where: {
+                    type: Number(query.type || 0)
+                },
+                join: [{
+                    tb: 'categories',
+                    leftKey: 'cid',
+                    rightKey: 'id'
+                }],
+                groupBy: 'cid',
+                order: {
+                    key: '`sum`',
+                    type: 'desc'
+                }
+            }, ctx);
+            if (ctx.SQL_SUCCESS) {
+                let result = select.map(item => {
+                    return {
+                        name: item.name,
+                        value: item.sum
+                    };
+                });
+                ctx.body = {
+                    code: 0,
+                    result: result
+                };
+            }
+        } else {
+            ctx.body = {
+                code: 0,
+                result: []
+            };
+        }
+    } else {
+        ctx.body = {
+            code: 0,
+            result: []
+        };
+    }
 });
 
 module.exports = route;
